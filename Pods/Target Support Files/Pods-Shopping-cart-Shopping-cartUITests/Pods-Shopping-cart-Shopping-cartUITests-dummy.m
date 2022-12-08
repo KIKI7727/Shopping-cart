ea1faf74be307dbfd3d108b94123ba388679a9ed
dongyu.cai <458498007@qq.com>
2022-12-08 16:18:16 +0800

    add Pods/

---
+++ Pods/Target Support Files/Pods-Shopping-cart-Shopping-cartUITests/Pods-Shopping-cart-Shopping-cartUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Shopping_cart_Shopping_cartUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Shopping_cart_Shopping_cartUITests
+@end
---
+++ Pods/Target Support Files/Pods-Shopping-cartTests/Pods-Shopping-cartTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Shopping_cartTests : NSObject
+@end
+@implementation PodsDummy_Pods_Shopping_cartTests
+@end
