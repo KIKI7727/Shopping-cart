8ae857190fffddec32f2ea940b4059172f1f2f61
dongyu.cai <458498007@qq.com>
2022-12-08 16:09:23 +0800

    deleted pods

--- Pods/Target Support Files/Pods-Shopping-cart-Shopping-cartUITests/Pods-Shopping-cart-Shopping-cartUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Shopping_cart_Shopping_cartUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Shopping_cart_Shopping_cartUITests
-@end
--- Pods/Target Support Files/Pods-Shopping-cart/Pods-Shopping-cart-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Shopping_cart : NSObject
-@end
-@implementation PodsDummy_Pods_Shopping_cart
-@end
